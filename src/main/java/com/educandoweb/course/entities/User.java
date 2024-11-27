1bece2cf5f0323337af7987ad917371d2aead217
Ayrton Itiro Kobo Yoshii <itiro_yoshii@hotmail.com>
2024-11-27 18:24:13 -0300

    Added Order, Instant, and ISO 8601 - 1/2

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,7 +3,10 @@ package com.educandoweb.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import com.educandoweb.course.entities.Order;
 
 @Entity
 @Table(name = "tb_user")
@@ -17,6 +20,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){ }
 
@@ -66,6 +72,10 @@ public class User implements Serializable {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
